1e2a7c84b8eac05fd26aa89519afd3b33155b538
ahatra5 <joao03richt@gmail.com>
2024-07-03 22:12:49 -0300

    calend e lua integrados

--- calendar.js
+++ calendar.js
@@ -1,55 +1,74 @@
-const daysTag = document.querySelector(".days"),
-    currentDate = document.querySelector(".current-date"),
-    prevNextIcon = document.querySelectorAll(".icons span");
+// const daysTag = document.querySelector(".days"),
+//     currentDate = document.querySelector(".current-date"),
+//     prevNextIcon = document.querySelectorAll(".icons span");
 
-// getting new date, current year and month
-let date = new Date(),
-    currYear = date.getFullYear(),
-    currMonth = date.getMonth();
+// // getting new date, current year and month
+// let date = new Date(),
+//     currYear = date.getFullYear(),
+//     currMonth = date.getMonth();
 
-// storing full name of all months in array
-const months = ["JANEIRO", "FEVEREIRO", "MARÇO", "ABRIL", "MAIO", "JUNHO", "JULHO",
-    "AGOSTO", "SETEMBRO", "OUTUBRO", "NOVEMBRO", "DEZEMBRO"];
+// // storing full name of all months in array
+// const months = ["JANEIRO", "FEVEREIRO", "MARÇO", "ABRIL", "MAIO", "JUNHO", "JULHO",
+//     "AGOSTO", "SETEMBRO", "OUTUBRO", "NOVEMBRO", "DEZEMBRO"];
 
-const renderCalendar = () => {
-    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
-        lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
-        lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
-        lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
-    let liTag = "";
+// // object to store specific days for each month and year
+// const purpleDays = {
+//     "2024": {
+//         "6": [5, 10, 15, 20], // Julho de 2024
+//         "7": [7, 14, 21, 28], // Agosto de 2024
+//         // adicione outros meses e anos conforme necessário
+//     },
+//     "2023": {
+//         "11": [1, 2, 3, 4], // Dezembro de 2023
+//         // adicione outros meses e anos conforme necessário
+//     }
+//     // adicione outros anos conforme necessário
+// };
 
-    for (let i = firstDayofMonth; i > 0; i--) { // creating li of previous month last days
-        liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
-    }
+// const renderCalendar = () => {
+//     let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
+//         lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
+//         lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
+//         lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
+//     let liTag = "";
 
-    for (let i = 1; i <= lastDateofMonth; i++) { // creating li of all days of current month
-        // adding active class to li if the current day, month, and year matched
-        let isToday = i === date.getDate() && currMonth === new Date().getMonth()
-            && currYear === new Date().getFullYear() ? "active" : "";
-        liTag += `<li class="${isToday}">${i}</li>`;
-    }
+//     for (let i = firstDayofMonth; i > 0; i--) { // creating li of previous month last days
+//         liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+//     }
 
-    for (let i = lastDayofMonth; i < 6; i++) { // creating li of next month first days
-        liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
-    }
-    currentDate.innerText = `${months[currMonth]} ${currYear}`; // passing current mon and yr as currentDate text
-    daysTag.innerHTML = liTag;
-}
-renderCalendar();
+//     for (let i = 1; i <= lastDateofMonth; i++) { // creating li of all days of current month
+//         // adding active class to li if the current day, month, and year matched
+//         let isToday = i === date.getDate() && currMonth === new Date().getMonth()
+//             && currYear === new Date().getFullYear() ? "active" : "";
 
-prevNextIcon.forEach(icon => { // getting prev and next icons
-    icon.addEventListener("click", () => { // adding click event on both icons
-        // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
-        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+//         // checking if the current day is in the purpleDays object
+//         let isPurple = purpleDays[currYear] && purpleDays[currYear][currMonth] && purpleDays[currYear][currMonth].includes(i) ? "purple" : "";
 
-        if (currMonth < 0 || currMonth > 11) { // if current month is less than 0 or greater than 11
-            // creating a new date of current year & month and pass it as date value
-            date = new Date(currYear, currMonth, new Date().getDate());
-            currYear = date.getFullYear(); // updating current year with new date year
-            currMonth = date.getMonth(); // updating current month with new date month
-        } else {
-            date = new Date(); // pass the current date as date value
-        }
-        renderCalendar(); // calling renderCalendar function
-    });
-});
+//         liTag += `<li class="${isToday} ${isPurple}">${i}</li>`;
+//     }
+
+//     for (let i = lastDayofMonth; i < 6; i++) { // creating li of next month first days
+//         liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
+//     }
+//     currentDate.innerText = `${months[currMonth]} ${currYear}`; // passing current mon and yr as currentDate text
+//     daysTag.innerHTML = liTag;
+// }
+// renderCalendar();
+
+// prevNextIcon.forEach(icon => { // getting prev and next icons
+//     icon.addEventListener("click", () => { // adding click event on both icons
+//         // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
+//         currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+
+//         if (currMonth < 0 || currMonth > 11) { // if current month is less than 0 or greater than 11
+//             // creating a new date of current year & month and pass it as date value
+//             date = new Date(currYear, currMonth, new Date().getDate());
+//             currYear = date.getFullYear(); // updating current year with new date year
+//             currMonth = date.getMonth(); // updating current month with new date month
+//         } else {
+//             date = new Date(); // pass the current date as date value
+//         }
+//         renderCalendar(); // calling renderCalendar function
+//     });
+// });
+
--- carousel.js
+++ carousel.js
@@ -1,25 +1,25 @@
-let slideAtual = 0;
-const totalSlides = 4;
-const carousel = document.querySelector('.carousel-inner');
-const images = document.querySelectorAll('.carousel-img');
+// let slideAtual = 0;
+// const totalSlides = 4;
+// const carousel = document.querySelector('.carousel-inner');
+// const images = document.querySelectorAll('.carousel-img');
 
-function reiniciar() {
-    const translateValue = -slideAtual * 100 + '%';
-    carousel.style.transform = 'translateX(' + translateValue + ')';
-}
+// function reiniciar() {
+//     const translateValue = -slideAtual * 100 + '%';
+//     carousel.style.transform = 'translateX(' + translateValue + ')';
+// }
 
 
-function changeSlide(direction) {
-    // Atualiza o slide atual baseado na direção fornecida
-    slideAtual += direction;
+// function changeSlide(direction) {
+//     // Atualiza o slide atual baseado na direção fornecida
+//     slideAtual += direction;
 
-    // Verifica se o slide atual ultrapassou os limites
-    if (slideAtual < 0) {
-        slideAtual = totalSlides - 1; // Define para o último slide
-    } else if (slideAtual >= totalSlides) {
-        slideAtual = 0; // Define para o primeiro slide
-    }
+//     // Verifica se o slide atual ultrapassou os limites
+//     if (slideAtual < 0) {
+//         slideAtual = totalSlides - 1; // Define para o último slide
+//     } else if (slideAtual >= totalSlides) {
+//         slideAtual = 0; // Define para o primeiro slide
+//     }
 
-    reiniciar();
-}
+//     reiniciar();
+// }
 
